4b250fe9f68dbbc9adadb84734a9097407456134
amirnakic <anakic1@etf.unsa.ba>
2018-11-13 04:27:38 +0100

    Korištenjem Scene Buildera kreiran je osnovni izgled digitrona.

--- src/ba/unsa/etf/rpr/tutorijal05/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Main.java
@@ -6,14 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("digitron.fxml"));
         primaryStage.setTitle("Digitron");
-        primaryStage.setScene(new Scene(root, 200, 275));
+        primaryStage.setScene(new Scene(root, 650, 545));
+        primaryStage.setResizable(true);
         primaryStage.show();
+
+
     }
 
 
