2a6445365cc0c6fe6c5473e6c19eaf7a8f5c6f0b
amirnakic <anakic1@etf.unsa.ba>
2018-11-13 04:47:07 +0100

    Realizovan je pristup poljima forme dodavanjem atributa unutar klase Controller.

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,26 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
 
+    public Button percentBtn;
+    public Button divideBtn;
+    public Button multiplyBtn;
+    public Button no1Btn;
+    public Button no2Btn;
+    public Button no3Btn;
+    public Button no4Btn;
+    public Button no5Btn;
+    public Button no6Btn;
+    public Button no7Btn;
+    public Button no8Btn;
+    public Button no9Btn;
+    public Label textLbl;
+    public Button minusBtn;
+    public Button plusBtn;
+    public Button equalsBtn;
+    public Button dotBtn;
+    public Button no0Btn;
 }
